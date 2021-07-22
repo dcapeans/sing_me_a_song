aba86bee727b5013fccec8b3e7845ebca1782abd
Daniel Capeans <dcapeans@gmail.com>
2021-07-22 03:54:32 -0300

    new recommendation route + tests

--- tests/integration/test.test.ts
+++ tests/integration/test.test.ts
@@ -1,11 +1,25 @@
 import '../../src/setup'
 import supertest from "supertest";
 import app from "../../src/app";
+import { clearDatabase, endConnection } from '../utils/database'
 
-describe("GET /test", () => {
-  it("should answer with text \"OK!\" and status 200", async () => {
-    const response = await supertest(app).get("/test");
-    expect(response.text).toBe("OK!");
-    expect(response.status).toBe(200);
+beforeEach(async () => {
+  await clearDatabase()
+})
+
+afterAll(async () => {
+  await endConnection()
+})
+
+describe("POST /recommendations", () => {
+  it("should answer status 201 when passed valid body", async () => {
+    const body = {name: "test", youtubeLink: "https://www.youtube.com/watch?v=testing"}
+
+    const response = await supertest(app).post("/recommendations").send(body);
+    expect(response.status).toBe(201);
+  });
+  it("should answer status 400 for invalid name or youtubeLink", async () => {
+    const response = await supertest(app).post("/recommendations");
+    expect(response.status).toBe(400);
   });
 });
